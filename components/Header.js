e852c54c66035acc6b1d05ed24420e44fb4903ac
Vettiger, Linus <linus.vettiger@novartis.com>
2019-05-24 17:19:29 +0200

    Implemented AntDesign

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,41 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import PropTypes from "prop-types";
+import { Menu, PageHeader } from "antd";
 
-const Header = () => (
-  <div>
-    <Link href="/"><a>Home</a></Link>
-    <Link href="/about"><a>About Me</a></Link>
-  </div>
-);
+const routes = {
+  '/': {
+    key: 'home',
+    label: 'Home'
+  },
+  '/about': {
+    key: 'about',
+    label: 'About'
+  }
+};
+
+const Header = () => {
+  const [path, setPath] = React.useState('/');
+  React.useEffect(() => {
+    const route = Router.router;
+    console.log(route.pathname);
+    setPath(route.pathname);
+  }, [Router.router]);
+
+  return (
+    <>
+      <PageHeader title={routes[path].label} onBack={() => history.back()}/>
+      <Menu mode="horizontal" selectedKeys={[routes[path].key]} >
+        {
+          Object.entries(routes).map(([ href, { key, label} ]) => (
+            <Menu.Item key={key}>
+              <Link href={href}><a>{label}</a></Link>
+            </Menu.Item>
+          ))
+        }
+      </Menu>
+    </>
+  );
+};
 
 export default Header;
