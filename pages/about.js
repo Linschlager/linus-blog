d8316c6ba2700f0fd0d1c244ad07e6ceb004536e
Linus Vettiger <linus4123@gmail.com>
2019-05-28 09:39:54 +0200

    Added github and stackoverflow links

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
+import {Icon} from "antd";
 
 
 const About = () => {
   return (
      <>
-        Hello, World!
-        This is Linus
+       <h1>Hi there, I'm Linus</h1>
+       <p>Find me on:</p>
+       <ul>
+         <li><Link href="https://github.com/Linschlager"><a target="_blank" rel="noreferrer"><Icon type="github" />&nbsp;Github</a></Link></li>
+         <li><Link href="https://stackoverflow.com/users/5228642"><a target="_blank" rel="noreferrer"><img alt="stackoverflow icon" src="https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico" height={14}/>&nbsp;StackOverflow</a></Link></li>
+       </ul>
      </>
   );
 };
