37904ec4ce62ad6ba821b9302d2bf3a023ca4019
Vettiger, Linus <linus.vettiger@novartis.com>
2019-06-06 17:46:38 +0200

    Implemented aggregation by year

--- pages/blog.js
+++ pages/blog.js
@@ -8,23 +8,44 @@ const postQuery = gql`
     posts {
       title
       slug
+      date
     }
   }
 `;
 const BlogPost = () => {
   return (
-    <Query query={postQuery}>
+    <Query query={postQuery} ssr>
     { ({ loading, error, data }) => {
       if (loading) return 'Loading...';
       if (error) return `Error: ${error.message}`;
+
+      const years = [];
+      const byYear = {};
+      data.posts.forEach(post => {
+        const y = new Date(post.date).getFullYear();
+        years.push(y);
+        if (!byYear[y]) byYear[y] = [];
+        byYear[y].push(post);
+      });
+
+
       return (
-        <ul>
+        <>
           {
-            data.posts.map(post => (
-              <li><Link href={`/post?slug=${post.slug}`}><a>{post.title}</a></Link></li>
+            years.map(year => (
+              <>
+              <h2>{ year }</h2>
+                <ul>
+                  {
+                    byYear[year].map(post => (
+                      <li key={post.slug}><Link href={`/post?slug=${post.slug}`}><a>{post.title}</a></Link></li>
+                    ))
+                  }
+                </ul>
+              </>
             ))
           }
-        </ul>
+        </>
       );
     }}
     </Query>
