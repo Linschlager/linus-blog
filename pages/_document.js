eebc5ad19998f9f787bb0e84bf71db705b1295fc
Vettiger, Linus <linus.vettiger@novartis.com>
2019-06-06 16:57:56 +0200

    Integrated Google Analytics Beacon

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,15 @@ class DocumentWrapper extends Document {
         <Head title={"Linus"}>
           <link rel="manifest" href="/static/manifest.json" />
           <link href="https://fonts.googleapis.com/css?family=Raleway:100|Roboto&display=swap" rel="stylesheet" />
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141582672-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-141582672-1');
+          </script>
         </Head>
         <body>
           <Main />
